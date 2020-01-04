e5258bf519dd55baf004274d20cf02e4602a360f
ajay143-ui <coonsingh@gmail.com>
2020-01-04 18:18:10 +0530

    this is ajay first branch

--- test.php
+++ test.php
@@ -3,9 +3,7 @@
 echo 'hello';
 echo '<br>';
 echo 'woow it\'s very nice';
-<<<<<<< HEAD
+
 print('<br>');
 echo "this from nilesh";
-=======
-echo 'https://github.com/ajay143-ui/learn/invitations';
->>>>>>> 572a880894c4a7086249eaa2a6fd3d400a4b6078
+
